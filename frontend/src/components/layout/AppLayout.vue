e2c8f32e8c1c388d6dc25359d20df96fcf979acb
XMX <3300245430@qq.com>
2025-10-29 16:39:19 +0800

    refactor: 完善布局组件

---
+++ frontend/src/components/layout/AppLayout.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+defineProps<{
+    containerClass?: string
+    headerClass?: string
+    mainClass?: string
+    footerClass?: string
+}>()
+</script>
+
+<template>
+    <div :class="['flex h-screen w-screen flex-col', containerClass]">
+        <div :class="['flex items-center justify-center', headerClass]">
+            <slot name="header" />
+        </div>
+        <div :class="['flex flex-1', mainClass]">
+            <slot name="sidebar" />
+            <slot name="content" />
+        </div>
+        <div :class="['flex items-center justify-center', footerClass]">
+            <slot name="footer" />
+        </div>
+    </div>
+</template>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,7 +1,24 @@
-<script lang="ts">
-
+<script setup lang="ts">
+import AppHeader from '@/components/layout/AppHeader.vue'
+import AppSidebar from '@/components/layout/AppSidebar.vue'
+import AppContent from '@/components/layout/AppContent.vue'
+import AppLayout from '@/components/layout/AppLayout.vue'
+import AppFooter from '@/components/layout/AppFooter.vue'
 </script>
 
 <template>
-
+    <AppLayout>
+        <template #header>
+            <AppHeader container-class="border rounded-lg border-gray-200 bg-cyan-40"></AppHeader>
+        </template>
+        <template #sidebar>
+            <AppSidebar container-class="border rounded-lg border-gray-200 bg-gray-100"></AppSidebar>
+        </template>
+        <template #content>
+            <AppContent container-class="border rounded-lg border-gray-200 bg-cyan-300"></AppContent>
+        </template>
+        <template #footer>
+            <AppFooter container-class="border rounded-lg border-gray-200 bg-red-100"></AppFooter>
+        </template>
+    </AppLayout>
 </template>
